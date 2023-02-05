43e2e0cd91e1cb601d9a8b2dad13a888bf815e97
jjcallah@ncsu.edu <jjattack164@gmail.com>
2023-02-05 06:36:02 -0500

    more UI dev

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -5,19 +5,48 @@ using UnityEngine.UI;
 
 public class ChangeText : MonoBehaviour
 {
+    public Sprite spriteTwo;
+    public Sprite spriteThree;
+    public Sprite spriteFour;
+
+
     public float num;
-    public Text textDisplay;
+    //public Text textDisplay;
     public GameObject slider;
+    private float index;
+    public SpriteRenderer renderer;
+    private Sprite newSprite;
 
     void Start()
     {
-        textDisplay.text = "2 Players";
+        slider.GetComponent<Slider>().value = 2;
     }
 
     public void textChange()
     {
         num = slider.GetComponent<Slider>().value;
-        textDisplay.text = "";
-        textDisplay.text = num.ToString() + " Players";
+        index = num + 51;
+        newSprite = null;
+        // textDisplay.text = "";
+        // textDisplay.text = num.ToString() + " Players";
+
+        switch( index )
+        {
+            case 53:
+                newSprite = spriteTwo; 
+                break;
+            case 54:
+                newSprite = spriteThree; 
+                break;
+            case 55:
+                newSprite = spriteFour; 
+                break;
+            default:
+                newSprite = spriteTwo;
+                break;
+        }
+
+        renderer.sprite = newSprite;
+        //renderer.transform.localScale = new Vector3( 800, 500, 0 );
     }
     }
